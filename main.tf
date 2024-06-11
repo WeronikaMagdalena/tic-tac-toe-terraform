1ddc7c5df9612e669a1b36b0dc4422e47cfa60f3
EC2 Default User <ec2-user@ip-172-31-49-143.ec2.internal>
2024-06-11 04:27:13 +0000

    working w/ public repo

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.0"
+      version = ">= 3.0"
     }
   }
   required_version = ">= 1.2.0"
@@ -13,104 +13,114 @@ provider "aws" {
 }
 
 resource "aws_vpc" "my_vpc" {
-  cidr_block = "10.0.0.0/16"
-  tags = {
-    Name        = "ticatctoe-vpc"
-    Terraform   = "true"
-    Environment = "dev"
-  }
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+}
+
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = aws_vpc.my_vpc.id
 }
 
 resource "aws_subnet" "public_subnet" {
   vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = "10.0.1.0/24"
+  cidr_block        = "10.0.101.0/24"
   availability_zone = "us-east-1b"
-  tags = {
-    Name        = "public-subnet"
-    Terraform   = "true"
-    Environment = "dev"
-  }
 }
 
-resource "aws_internet_gateway" "igw" {
+resource "aws_route_table" "public_route_table" {
   vpc_id = aws_vpc.my_vpc.id
-  tags = {
-    Name        = "my-vpc-igw"
-    Terraform   = "true"
-    Environment = "dev"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my_igw.id
   }
 }
 
-resource "aws_route_table" "public_rt" {
-  vpc_id = aws_vpc.my_vpc.id
-  tags = {
-    Name        = "public-route-table"
-    Terraform   = "true"
-    Environment = "dev"
-  }
-}
-
-resource "aws_route" "default_route" {
-  route_table_id         = aws_route_table.public_rt.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.igw.id
-}
-
-resource "aws_route_table_association" "public_rt_assoc" {
+resource "aws_route_table_association" "public_subnet_association" {
   subnet_id      = aws_subnet.public_subnet.id
-  route_table_id = aws_route_table.public_rt.id
+  route_table_id = aws_route_table.public_route_table.id
 }
 
-resource "aws_security_group" "tictactoe_sg" {
-  vpc_id = aws_vpc.my_vpc.id
+resource "aws_security_group" "allow_ssh_http" {
+  name        = "allow_ssh_http"
+  description = "Allow SSH and HTTP inbound traffic and all outbound traffic"
+  vpc_id      = aws_vpc.my_vpc.id
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 8080
-    to_port     = 8081
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+  tags = {
+    Name = "allow-ssh-http"
   }
 }
 
-resource "aws_secretsmanager_secret" "github_key" {
-  name = "myproject/privkey"
-  recovery_window_in_days = 0
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.allow_ssh_http.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # all ports
+}
+resource "aws_vpc_security_group_ingress_rule" "allow_http" {
+  security_group_id = aws_security_group.allow_ssh_http.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "tcp"
+  from_port         = 8080
+  to_port           = 8081
+}
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
+  security_group_id = aws_security_group.allow_ssh_http.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "tcp"
+  from_port         = 22
+  to_port           = 22
 }
 
-resource "aws_secretsmanager_secret_version" "github_key_value" {
-secret_id = aws_secretsmanager_secret.github_key.id
-secret_string = file("repo_key")
-}
 
-resource "aws_instance" "webserver" {
-  ami                         = "ami-00beae93a2d981137"
+resource "aws_instance" "tf-web-server" {
+  ami                         = "ami-08a0d1e16fc3f61ea"
   instance_type               = "t2.micro"
   key_name                    = "vockey"
   subnet_id                   = aws_subnet.public_subnet.id
-  associate_public_ip_address = true
-  vpc_security_group_ids      = [aws_security_group.tictactoe_sg.id]
+  associate_public_ip_address = "true"
+  vpc_security_group_ids      = [aws_security_group.allow_ssh_http.id]
+  user_data                   = <<-EOF
+#!/bin/bash
+sudo yum install -y docker
+sudo yum install -y git
+sudo systemctl start docker
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.12.2/docker-compose-$(uname -s)-$(uname -m)"  -o /usr/local/bin/docker-compose
+sudo mv /usr/local/bin/docker-compose /usr/bin/docker-compose
+sudo chmod +x /usr/bin/docker-compose
 
+API_URL="http://169.254.169.254/latest/api"
+TOKEN=$(curl -X PUT "$API_URL/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 600")
+TOKEN_HEADER="X-aws-ec2-metadata-token: $TOKEN"
+METADATA_URL="http://169.254.169.254/latest/meta-data"
+AZONE=$(curl -H "$TOKEN_HEADER" -s $METADATA_URL/placement/availability-zone)
+IP_V4=$(curl -H "$TOKEN_HEADER" -s $METADATA_URL/public-ipv4)
+INTERFACE=$(curl -H "$TOKEN_HEADER" -s $METADATA_URL/network/interfaces/macs/ | head -n1)
+SUBNET_ID=$(curl -H "$TOKEN_HEADER" -s $METADATA_URL/network/interfaces/macs/$INTERFACE/subnet-id)
+VPC_ID=$(curl -H "$TOKEN_HEADER" -s $METADATA_URL/network/interfaces/macs/$INTERFACE/vpc-id)
+
+echo "Your EC2 instance works in: AvailabilityZone: $AZONE, VPC: $VPC_ID, VPC subnet: $SUBNET_ID, IP address: $IP_V4"
+
+sudo chmod a+w /tmp
+
+echo "$IP_V4" | sudo tee /tmp/ec2_ip_address.txt
+# echo "$IP_V4" > /tmp/ec2_ip_address.txt
+
+rm -rf a5-WeronikaMagdalena
+git clone https://github.com/pwr-cloudprogramming/a5-WeronikaMagdalena
+
+cd a5-WeronikaMagdalena
+
+docker-compose build --build-arg ip="$IP_V4" --no-cache
+
+docker-compose up -d
+
+EOF
   user_data_replace_on_change = true
-
   tags = {
-    Name = "TicTacToe-Web-Server"
+    Name = "TicTacToe-WW"
   }
-  
-  # This resource contains arguments related to instruction only
-  iam_instance_profile = "LabInstanceProfile"
-  user_data = file("setup.sh")
 }
--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# This script contains command related to instruction only
-apt-get update -y
-apt-get install -y awscli
-HOME=/home/ubuntu
+                
+sudo yum update -y
+sudo yum install -y awscli git docker
+HOME=/home/ec2-user
 aws secretsmanager get-secret-value --region us-east-1 \
 --secret-id "myproject/privkey" \
 --query "SecretString" \
@@ -13,35 +13,37 @@ Host github.com
 Hostname github.com
 IdentityFile=~/.ssh/repo_key.pem
 EOF
+
 ssh-keyscan github.com >> $HOME/.ssh/known_hosts
-chown ubuntu:ubuntu $HOME/.ssh/*
-su - ubuntu -c "cd ; git clone git@github.com:pwr-cloudprogramming/a5-WeronikaMagdalena.git"
 
-#!/bin/bash
-# METADATA_URL="http://169.254.169.254/latest/meta-data"
-# IP_V4=$(curl -H "$TOKEN_HEADER" -s $METADATA_URL/public-ipv4)
+sudo chown ec2-user:ec2-user $HOME/.ssh/*
 
-# sudo yum update
-# sudo yum install -y git
+rm -rf a5-WeronikaMagdalena
 
-# sudo chmod a+w /tmp
+sudo su - ec2-user -c "git clone git@github.com:pwr-cloudprogramming/a5-WeronikaMagdalena.git"
 
-# echo "$IP_V4" | sudo tee /tmp/ec2_ip_address.txt
+cd home/ec2-user/a5-WeronikaMagdalena/
 
-# rm -rf a5-WeronikaMagdalena
-# git clone git@github.com:pwr-cloudprogramming/a5-WeronikaMagdalena.git
-# cd a5-WeronikaMagdalena
 
-# sudo yum install -y stress-ng
+sudo systemctl start docker
+sudo usermod -aG docker $USER
+newgrp docker
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.12.2/docker-compose-$(uname -s)-$(uname -m)"  -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
-# sudo systemctl start docker
-# sudo groupadd docker
-# sudo usermod -aG docker $USER
-# newgrp docker
-# sudo curl -L "https://github.com/docker/compose/releases/download/v2.12.2/docker-compose-$(uname -s)-$(uname -m)"  -o /usr/local/bin/docker-compose
-# sudo mv /usr/local/bin/docker-compose /usr/bin/docker-compose
-# sudo chmod +x /usr/bin/docker-compose
+API_URL="http://169.254.169.254/latest/api"
+TOKEN=`curl -X PUT "$API_URL/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 600"` 
+TOKEN_HEADER="X-aws-ec2-metadata-token: $TOKEN"
+METADATA_URL="http://169.254.169.254/latest/meta-data"
+AZONE=`curl -H "$TOKEN_HEADER" -s $METADATA_URL/placement/availability-zone`
+IP_V4=`curl -H "$TOKEN_HEADER" -s $METADATA_URL/public-ipv4`
+INTERFACE=`curl -H "$TOKEN_HEADER" -s $METADATA_URL/network/interfaces/macs/ | head -n1`
+SUBNET_ID=`curl -H "$TOKEN_HEADER" -s $METADATA_URL/network/interfaces/macs/${INTERFACE}/subnet-id`
+VPC_ID=`curl -H "$TOKEN_HEADER" -s $METADATA_URL/network/interfaces/macs/${INTERFACE}/vpc-id`
+echo "Your EC2 instance works in :AvailabilityZone: ${AZONE} / VPC: ${VPC_ID} / VPC subnet: ${SUBNET_ID} / IP address: ${IP_V4}"
+sudo chmod a+w /tmp
+echo "$IP_V4" | sudo tee /tmp/ec2_ip_address.txt
 
-# docker-compose build --build-arg ip="$IP_V4" --no-cache
+docker-compose build --build-arg ip="$IP_V4" --no-cache
+docker-compose up -d
 
-# docker-compose up -d
